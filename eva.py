1ffb30ad0e0739a99a01468f0c4b4e9287975b1b
Ahmad Tameem <ahmad.tameem97@gmail.com>
2023-03-16 13:50:05 +0500

    Operator Expressions

--- eva.py
+++ eva.py
@@ -6,7 +6,7 @@ class Eva_Interpreter():
         pass
 
 
-    def eval(self, exp):
+    def eval(self, exp, ent=0):
         # Self Evaluating Expression (int)
         if (type(exp) == int):
             return exp
@@ -15,6 +15,25 @@ class Eva_Interpreter():
         # Self Evaluating Expression (str)
         if (type(exp) == str and exp[0]=="\"" and exp[-1]=="\""):
             return exp[1:-1]
+        
+
+        # Operator Expressions
+        if (exp[0] == "+"):  return self.eval(exp[1], ent) + self.eval(exp[2], ent)
+        if (exp[0] == "-"):  return self.eval(exp[1], ent) - self.eval(exp[2], ent)
+        if (exp[0] == "*"):  return self.eval(exp[1], ent) * self.eval(exp[2], ent)
+        if (exp[0] == "/"):  return self.eval(exp[1], ent) / self.eval(exp[2], ent)
+        if (exp[0] == "//"): return self.eval(exp[1], ent) // self.eval(exp[2], ent)
+        if (exp[0] == "**"): return self.eval(exp[1], ent) ** self.eval(exp[2], ent)
+
+        if (exp[0] == ">"):  return self.eval(exp[1], ent) > self.eval(exp[2], ent)
+        if (exp[0] == "<"):  return self.eval(exp[1], ent) < self.eval(exp[2], ent)
+        if (exp[0] == ">="):  return self.eval(exp[1], ent) >= self.eval(exp[2], ent)
+        if (exp[0] == "<="):  return self.eval(exp[1], ent) <= self.eval(exp[2], ent)        
+        if (exp[0] == "=="):  return self.eval(exp[1], ent) == self.eval(exp[2], ent)
+
+        if (exp[0] == "and"):  return self.eval(exp[1], ent) and self.eval(exp[2], ent)
+        if (exp[0] == "or"):  return self.eval(exp[1], ent) or self.eval(exp[2], ent)
+        if (exp[0] == "not"):  return not self.eval(exp[1], ent)
         
 
         raise ValueError("Unimplemented")
--- main.py
+++ main.py
@@ -9,3 +9,9 @@ Interpreter = Eva_Interpreter()
 test(Interpreter.eval(9), 9, "Self Evaluating Expression Failed")
 test(Interpreter.eval(100), 100, "Self Evaluating Expression Failed")
 test(Interpreter.eval("\"Tameem\""), "Tameem", "Self Evaluating Expression Failed")
+
+
+# Math Operations Test
+test(Interpreter.eval(["+", 90, 9]), 99, "Addition Expression Failed")
+test(Interpreter.eval(["+", ["+", 7, 3], 90]), 100, "Addition Expression Failed")
+test(Interpreter.eval(["+", ["-", 81, ["*", 9, 9]], 90]), 90, "Addition Expression Failed")
