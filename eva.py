25cfe2e900c897c003b14aeee11fb5afa84a4567
Ahmad Tameem <ahmad.tameem97@gmail.com>
2023-03-16 13:45:26 +0500

    Self Evaluating Expressions

--- eva.py
+++ eva.py
@@ -5,5 +5,16 @@ class Eva_Interpreter():
     def __init__(self) -> None:
         pass
 
+
     def eval(self, exp):
+        # Self Evaluating Expression (int)
+        if (type(exp) == int):
+            return exp
+        
+
+        # Self Evaluating Expression (str)
+        if (type(exp) == str and exp[0]=="\"" and exp[-1]=="\""):
+            return exp[1:-1]
+        
+
         raise ValueError("Unimplemented")
--- main.py
+++ main.py
@@ -5,4 +5,7 @@ from test import test
 Interpreter = Eva_Interpreter() 
 
 
-
+# Self Evaluating Expression Test
+test(Interpreter.eval(9), 9, "Self Evaluating Expression Failed")
+test(Interpreter.eval(100), 100, "Self Evaluating Expression Failed")
+test(Interpreter.eval("\"Tameem\""), "Tameem", "Self Evaluating Expression Failed")
