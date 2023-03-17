17056ffeef970a212a673b26798c3c9baa2b3237
Ahmad Tameem <ahmad.tameem97@gmail.com>
2023-03-17 15:15:48 +0500

    Modified Constructor

--- environment.py
+++ environment.py
@@ -2,30 +2,36 @@
 # Repository for variables and functions defined in the program
 
 class env:
-    def __init__(self, parent=None) -> None:
+    def __init__(self, parent=None, record=None) -> None:
         self.record = {'True': True, 'False': False, 'None': None, 'print': self.funct1}
+        if (record != None): self.record = {**self.record, **record}
         self.parent = parent
 
 
+    # Built-in Print Function
     def funct1(self, *args):
         print(*args)
         return None
 
 
+    # Defines a variable
     def define(self, name, value):
         self.record[name] = value
         return value
 
 
+    # Assign a Value to a variable 
     def assign(self, name, value):
         self.resolve(name).record[name] = value
         return value
 
     
+    # Search variable 
     def lookup(self, name):
         return self.resolve(name).record.get(name)
 
 
+    # Manages Scope Resolution for variables
     def resolve(self, name):
         if (self.record.get(name) != None):
             return self
